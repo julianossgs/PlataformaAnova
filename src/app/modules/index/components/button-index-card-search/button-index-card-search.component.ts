19a5df86b9df78d5375f584b8fb32eab34726886
Juliano Candido <julianosgs@yahoo.com.br>
2025-01-31 18:08:57 -0300

    Atualizações do dia 31/01/2025

--- src/app/modules/index/components/button-index-card-search/button-index-card-search.component.ts
+++ src/app/modules/index/components/button-index-card-search/button-index-card-search.component.ts
@@ -1,18 +1,31 @@
-import { Component,Input,Output,EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { DateRangePickerComponent } from '../date-range-picker/date-range-picker.component';
 import { IconIndexComponent } from '../icon-index/icon-index.component';
 
 @Component({
-    selector: 'app-button-index-card-search',
-    imports: [IconIndexComponent],
-    templateUrl: './button-index-card-search.component.html',
-    styleUrl: './button-index-card-search.component.css'
+  selector: 'app-button-index-card-search',
+  standalone: true,
+  imports: [CommonModule, IconIndexComponent],
+  templateUrl: './button-index-card-search.component.html',
+  styleUrl: './button-index-card-search.component.css'
 })
 export class ButtonIndexCardSearchComponent {
-      onButtonClick(){
-        alert('teste botão')
-      }
+  @Input() iconClass: string = '';
+  @Input() iconColor: string = '';
+  @Input() iconSize: string = '';
+  @Output() dateSelected = new EventEmitter<{ dataInicial: string, dataFinal: string }>();
 
-      @Input() iconClass:string='';
-      @Input() iconColor:string='';
-      @Input() iconSize:string='';
+  constructor(private modalService: NgbModal) {}
+
+  onButtonClick() {
+    const modalRef = this.modalService.open(DateRangePickerComponent, { size: 'sm', centered: true });
+    modalRef.result.then((result) => {
+      if (result) {
+        console.log('Datas selecionadas:', result);
+        this.dateSelected.emit(result);
+      }
+    }).catch(() => {});
+  }
 }
--- src/app/modules/index/components/card-index-search/card-index-search.component.ts
+++ src/app/modules/index/components/card-index-search/card-index-search.component.ts
@@ -6,11 +6,12 @@ import { InputSelectIndexCardSearchComponent } from "../input-select-index-card-
     selector: 'app-card-index-search',
     imports: [
         ButtonIndexCardSearchComponent,
-        InputSelectIndexCardSearchComponent
+        InputSelectIndexCardSearchComponent,
+
     ],
     templateUrl: './card-index-search.component.html',
     styleUrl: './card-index-search.component.css'
 })
 export class CardIndexSearchComponent {
-
+  
 }
